69e3c5b34f6b7c5a67669d04668ca3872c180a55
Ryusake Weiss <ryuweiss@gmail.com>
2020-01-15 11:51:16 +0100

    ComboSystem Working

---
+++ Despresso/Assets/Scenes/Steven/Classes/ComboSystem/ComboHolder.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComboHolder : MonoBehaviour
+{
+    public string lightSlashInput;
+    public string heavySlashInput;
+
+    public Slash curSlash;
+
+    public Slash lightSlash;
+    public Slash heavySlash;
+
+    public bool inCombo;
+
+    public AttackInput lightAttackInputPressed;
+    public AttackInput heavyAttackInputPressed;
+
+    public void Update()
+    {
+        InputCheck();
+    }
+
+    public void InputCheck()
+    {
+        if (inCombo)
+        {
+            if (Input.GetButtonDown(lightSlashInput))
+            {    
+                curSlash.ContinueAttack(lightAttackInputPressed, this, lightSlash);
+            }
+            else if (Input.GetButtonDown(heavySlashInput))
+            {
+                curSlash.ContinueAttack(heavyAttackInputPressed, this, heavySlash);
+            }
+        }
+        else
+        {
+            if (Input.GetButtonDown(lightSlashInput))
+            {
+                curSlash = lightSlash;
+                NewAttack(curSlash);
+            }
+
+            if (Input.GetButtonDown(heavySlashInput))
+            {
+                curSlash = heavySlash;
+                NewAttack(curSlash);
+            }
+        }
+    }
+
+    public void NewAttack(Slash slash)
+    {
+        curSlash.NewAttack(this, lightSlash);
+    }
+
+}
+    public enum AttackInput
+    {
+        lightAttack,
+        heavyAttack
+    }
---
+++ Despresso/Assets/Scenes/Steven/Classes/ComboSystem/Slash.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = ("Slash"), menuName = "Slash/New Slash")]
+public class Slash : ScriptableObject
+{
+    public List<Slash> combos;
+
+    public int damage;
+
+    public AttackInput attackInput;
+
+    public virtual void NewAttack(ComboHolder comboHolder, Slash slash)
+    {
+        comboHolder.curSlash = slash;
+
+        comboHolder.inCombo = true;
+
+        Debug.Log(comboHolder.curSlash.damage);
+    }
+
+    public virtual void ContinueAttack(AttackInput attack, ComboHolder combo, Slash slash)
+    {
+        for(int i = 0; i < combos.Count; i++)
+        {
+            if(attack == combos[i].attackInput)
+            {
+                NewAttack(combo, combos[i]);
+                return;
+            }
+        }
+
+        NewAttack(combo, slash);
+    }
+}
